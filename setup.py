22183e6adfbb80e1c1bc99ebf3de9748765e8979
ww <ww@styx.org>
2011-02-17 14:04:03 +0000

    fix version check to actually work with different python versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [
     "owslib",
     "lxml",
 ]
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+if not sys.version.startswith("2.7") and not sys.version.startswith("3"):
     requirements.append("argparse")
     
 setup(
